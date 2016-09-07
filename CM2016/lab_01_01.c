c287b799de398ba00d2f65a42c1c6e27e3151530
syphuongqd <syphuong2596@gmail.com>
2016-09-07 13:57:45 +0200

    Created by Coding Ground Client

---
+++ CM2016/lab_01_01.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void main ()
+{int x=6;
+int y=8;
+printf("x+y= %d \n", x+y);
+printf("x-y= %d \n",x-y);
+printf("x*y= %d \n", x*y);
+printf("x %% y= %d \n", x%y);
+printf("x/y= %d \n", x/y);
+double a=5.6;
+double b=6.5;
+printf("a/b= %lf \n",a/b);
+printf("x+a= %lf \n", x+a);
+char c1=10;
+char c2=100;
+printf("c1+c2= %d \n",c1+c2);
+int i=15;
+printf("i= %d \n",i);
+i=i+1;
+printf("i=i+1:%d \n",i);
+i++;
+printf("i++: %d \n",i);
+i+=1;
+printf("i+=1: %d \n",i);
+
+}
