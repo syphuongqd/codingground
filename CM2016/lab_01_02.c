a3ac231698389a75e6db10617883564536c43924
syphuongqd <syphuong2596@gmail.com>
2016-09-07 13:57:58 +0200

    Created by Coding Ground Client

---
+++ CM2016/lab_01_02.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+    char c1=100,
+    c2= 't';
+    printf("c1=%d \n",c1);
+    printf("c1=%c \n",c1);
+    printf("c2=%d \n",c2);
+    const int coef =2016;
+    double a=1005.23125478965,
+    b=5.347;
+    printf("%7d \n",coef);
+    printf("%7.11f\n",b);
+    
+    
+}
